f0c5a221cda771c4040b52f407bcdb631dfef150
KhangKoii <hpgiakhang.personal@gmail.com>
2025-07-30 18:58:06 +0700

    fix status code for verify user middleware

--- backend/src/middlewares/verifyUserMiddleware.js
+++ backend/src/middlewares/verifyUserMiddleware.js
@@ -7,14 +7,14 @@ export const verifyUserMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization
 
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return next(new ApiError(StatusCodes.UNAUTHORIZED, 'Invalid or expired access token'))
+    return next(new ApiError(StatusCodes.FORBIDDEN, 'Invalid or expired access token'))
   }
 
   const token = authHeader.split(' ')[1]
 
   jwt.verify(token, ENV.JWT_ACCESS_SECRET, (err, decoded) => {
     if (err) {
-      return next(new ApiError(StatusCodes.UNAUTHORIZED, 'Invalid or expired access token'))
+      return next(new ApiError(StatusCodes.FORBIDDEN, 'Invalid or expired access token'))
     }
 
     req.user = decoded
